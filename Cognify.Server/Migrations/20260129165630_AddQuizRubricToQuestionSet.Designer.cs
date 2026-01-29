c3ec86ff32227b4321387eaaf83cc56b7ead40ff
norosz <kulonnor@gmail.com>
2026-01-29 17:59:19 +0100

    added Rubric class and migrations

--- Cognify.Server/Migrations/20260129165630_AddQuizRubricToQuestionSet.Designer.cs
+++ Cognify.Server/Migrations/20260129165630_AddQuizRubricToQuestionSet.Designer.cs
@@ -4,6 +4,7 @@ using Cognify.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cognify.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129165630_AddQuizRubricToQuestionSet")]
+    partial class AddQuizRubricToQuestionSet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
